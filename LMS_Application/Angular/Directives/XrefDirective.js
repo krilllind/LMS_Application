4f0a13f5091007ac29a55835a468b306059b4f84
Kristoffer Lindström <krillllind@gmail.com>
2017-01-10 00:21:44 +0100

    hyperlink directive

---
+++ LMS_Application/Angular/Directives/XrefDirective.js
@@ -0,0 +1,28 @@
+﻿(function () {
+
+    var xref = function ($location, $route) {
+        var bind = function (scope, el, attr) {
+            el.on("click", function () {
+                scope.$apply(function () {
+                    if ($location.url() == attr.xref) {
+                        $route.reload();
+                    }
+                    else {
+                        $location.path(attr.xref);
+                    }
+                });
+            });
+        }
+
+        return {
+            link: bind
+        }
+    }
+
+    LMSApp.directive("xref", [
+        "$location",
+        "$route",
+        xref
+    ]);
+
+}());
