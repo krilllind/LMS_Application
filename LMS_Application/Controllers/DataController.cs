3a512ca192679b817523bda28284ec38beee4e68
Phixle <henry.ohman@yahoo.se>
2017-01-15 22:07:46 +0100

    Moved GetSchedule to ScheduleController

--- LMS_Application/Controllers/DataController.cs
+++ LMS_Application/Controllers/DataController.cs
@@ -99,42 +99,5 @@ namespace LMS_Application.Controllers
 
             return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, "Could not remove school class");
         }
-
-        [HttpGet]
-        [ValidateAngularAntiForgery]
-        public string GetSchedule()
-        {
-            List<object> test = new List<object>() {
-                new {
-                    From = "10:30",
-                    To = "12:00",
-                    Day = "Tuesday",
-                    LessonType = "English",
-                    Color = "lightblue",
-                    Teacher = "TLUG",
-                    Classroom = "C320"
-                },
-                new {
-                    From = "10:30",
-                    To = "15:20",
-                    Day = "Friday",
-                    LessonType = "Programming",
-                    Color = "pink",
-                    Teacher = "ELÖV",
-                    Classroom = "D220"
-                },
-                new {
-                    From = "08:30",
-                    To = "09:45",
-                    Day = "Monday",
-                    LessonType = "Math",
-                    Color = "lightgreen",
-                    Teacher = "POLV",
-                    Classroom = "A332"
-                }
-            };
-
-            return JsonConvert.SerializeObject(test, Formatting.None, _jsonSettings);
-        }
     }
 }
