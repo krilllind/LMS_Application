78a59b0233f23de94fa0a4c2e615c2447d6c3072
Kristoffer <krillllind@gmail.com>
2017-01-17 14:17:18 +0100

    assdfsdfdf

---
+++ LMS_Application/Migrations/201701171313292_lastMig.cs
@@ -0,0 +1,16 @@
+namespace LMS_Application.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class lastMig : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
